0d2e20b7158e46ab39e2205011797d04ece853e8
howhow <howhow0605@qq.com>
2025-10-07 14:57:42 +0800

    modify train script

--- AHBF-pytorch/train_ahbf.py
+++ AHBF-pytorch/train_ahbf.py
@@ -58,6 +58,10 @@ parser.add_argument('--lambda2', default=1.0, type=float)
 parser.add_argument('--lambda1', default=1.0, type=float)
 parser.add_argument('--att_type', default='conv', type=str, help='use cbam, se, nonlocal to employ different attention mechanism: default(conv)')
 parser.add_argument('--use_adaptive_weighting', default=True, type=bool, help='use adaptive weighting based on cosine similarity: default(True)')
+parser.add_argument('--use_contrastive_learning', default=True, type=bool, help='use contrastive learning with InfoNCE loss: default(True)')
+parser.add_argument('--contrastive_weight', default=0.1, type=float, help='weight for contrastive learning loss: default(0.1)')
+parser.add_argument('--contrastive_temp', default=0.1, type=float, help='temperature parameter for contrastive learning: default(0.1)')
+parser.add_argument('--center_momentum', default=0.6, type=float, help='momentum for online center update: default(0.6)')
 
 parser.add_argument('--wandb_notes', default='', type=str)
 parser.add_argument('--notes', default='', type=str)
@@ -96,6 +100,7 @@ def train(train_loader, model, optimizer, criterion, criterion_T, accuracy, args
     loss_true_avg = utils.RunningAverage()
     loss_group_ekd_avg = utils.RunningAverage()
     loss_group_dkd_avg = utils.RunningAverage()
+    loss_contrastive_avg = utils.RunningAverage()
     loss_avg = utils.RunningAverage()
     end = time.time()
 
@@ -105,7 +110,12 @@ def train(train_loader, model, optimizer, criterion, criterion_T, accuracy, args
             train_batch = train_batch.to(device)
             labels_batch = labels_batch.to(device)
 
-            logitlist, ensem_logits = model(train_batch)
+            # 根据模型类型选择是否传递labels参数（仅对resnet传递）
+            if "resnet" in args.model:
+                logitlist, ensem_logits, contrastive_loss = model(train_batch, labels=labels_batch)
+            else:
+                logitlist, ensem_logits = model(train_batch)
+                contrastive_loss = torch.tensor(0.0, device=device)
             loss_true = 0
             loss_group_ekd = 0
             loss_group_dkd = 0
@@ -128,11 +138,14 @@ def train(train_loader, model, optimizer, criterion, criterion_T, accuracy, args
             #     loss_group_ekd +=  criterion_T(logitlist[i + 1], ensem_logits[i]) * args.kd_weight * rampup_weight * args.lambda1
             #     loss_group_ekd +=  criterion_T(ensem_logits[i - 1], ensem_logits[i]) * args.kd_weight * rampup_weight * args.lambda1
 
-            loss = loss_true +  loss_group_dkd+loss_group_ekd
+            # 添加对比学习损失
+            contrastive_weighted_loss = contrastive_loss * args.contrastive_weight
+            loss = loss_true + loss_group_dkd + loss_group_ekd + contrastive_weighted_loss
 
             loss_true_avg.update(loss_true.item())
             loss_group_ekd_avg.update(loss_group_ekd.item())
             loss_group_dkd_avg.update(loss_group_dkd.item())
+            loss_contrastive_avg.update(contrastive_loss.item())
             loss_avg.update(loss.item())
 
             # Update average loss and accuracy
@@ -160,12 +173,14 @@ def train(train_loader, model, optimizer, criterion, criterion_T, accuracy, args
                      'train_true_loss': loss_true_avg.value(),
                      'train_group_ekd_loss': loss_group_ekd_avg.value(),
                      'train_group_dkd_loss': loss_group_dkd_avg.value(),
+                     'train_contrastive_loss': loss_contrastive_avg.value(),
                      'train_accTop1_target': accTop1_avg[0].value(),
                      'time': time.time() - end}
     wandb.log({'trainloss': loss_avg.value(),
                'trainlossce': loss_true_avg.value(),
                'trainloss_ekd':loss_group_ekd_avg.value(),
-               'trainloss_dkd':loss_group_dkd_avg.value()})
+               'trainloss_dkd':loss_group_dkd_avg.value(),
+               'trainloss_contrastive':loss_contrastive_avg.value()})
     train_metrics.update({'train_acc_target' : accTop1_avg[0].value()})
     wandb.log({'train_acc_target' : accTop1_avg[0].value()})
 
@@ -198,6 +213,7 @@ def evaluate(test_loader, model, criterion, criterion_T, accuracy, args, rampup_
     loss_true_avg = utils.RunningAverage()
     loss_group_ekd_avg = utils.RunningAverage()
     loss_group_dkd_avg = utils.RunningAverage()
+    loss_contrastive_avg = utils.RunningAverage()
     loss_avg = utils.RunningAverage()
     # dist_avg = utils.RunningAverage()
     end = time.time()
@@ -211,7 +227,12 @@ def evaluate(test_loader, model, criterion, criterion_T, accuracy, args, rampup_
             loss_true = 0
             loss_group_dkd = 0
             loss_group_ekd = 0
-            logitlist, ensem_logits = model(test_batch)
+            # 根据模型类型选择是否传递labels参数（仅对resnet传递）
+            if "resnet" in args.model:
+                logitlist, ensem_logits, contrastive_loss = model(test_batch, labels=labels_batch)
+            else:
+                logitlist, ensem_logits = model(test_batch)
+                contrastive_loss = torch.tensor(0.0, device=device)
 
             for output in logitlist:
                 loss_true +=   criterion(output, labels_batch)
@@ -227,13 +248,14 @@ def evaluate(test_loader, model, criterion, criterion_T, accuracy, args, rampup_
                     loss_group_ekd +=  criterion_T(logitlist[i + 1], ensem_logits[i]) * args.kd_weight * rampup_weight * args.lambda1*0.5
                     loss_group_ekd +=  criterion_T(ensem_logits[i - 1], ensem_logits[i]) * args.kd_weight * rampup_weight * args.lambda1*0.5
 
-
-
-            loss = loss_true +  loss_group_dkd+loss_group_ekd
+            # 添加对比学习损失（评估时权重为0，不影响总损失）
+            contrastive_weighted_loss = contrastive_loss * args.contrastive_weight
+            loss = loss_true + loss_group_dkd + loss_group_ekd + contrastive_weighted_loss
 
             loss_true_avg.update(loss_true.item())
             loss_group_ekd_avg.update(loss_group_ekd.item())
             loss_group_dkd_avg.update(loss_group_dkd.item())
+            loss_contrastive_avg.update(contrastive_loss.item())
             loss_avg.update(loss.item())
 
             # Update average loss and accuracy
@@ -251,9 +273,11 @@ def evaluate(test_loader, model, criterion, criterion_T, accuracy, args, rampup_
                      'test_true_loss': loss_true_avg.value(),
                      'test_group_ekd_loss': loss_group_ekd_avg.value(),
                      'test_group_dkd_loss': loss_group_dkd_avg.value(),
+                     'test_contrastive_loss': loss_contrastive_avg.value(),
                      'test_accTop1_target': accTop1_avg[0].value(),
                      'time': time.time() - end}
-    wandb.log({'testloss': loss_avg.value()})  # measure elapsed time
+    wandb.log({'testloss': loss_avg.value(),
+               'testloss_contrastive': loss_contrastive_avg.value()})  # measure elapsed time
     test_metrics.update({'test_acc_target' : accTop1_avg[0].value()})
     wandb.log({'test_acc_target' : accTop1_avg[0].value()})
 
@@ -323,7 +347,13 @@ def train_and_evaluate(model, train_loader, test_loader, optimizer, criterion, c
         test_metrics = evaluate(test_loader, model, criterion, criterion_T, accuracy, args, rampup_weight)
 
         test_acc = test_metrics['test_accTop1_target']
-
+        
+        # 在epoch结束时更新历史融合输出和类中心（仅对resnet）
+        if "resnet" in args.model and hasattr(model, 'update_epoch_history'):
+            if torch.cuda.device_count() > 1:
+                model.module.update_epoch_history()
+            else:
+                model.update_epoch_history()
 
         result_train_metrics[epoch] = train_metrics
         result_test_metrics[epoch] = test_metrics
@@ -425,6 +455,13 @@ if __name__ == '__main__':
     # 设置自适应加权参数
     if hasattr(model, 'use_adaptive_weighting'):
         model.use_adaptive_weighting = args.use_adaptive_weighting
+    
+    # 设置对比学习参数（仅对resnet）
+    if "resnet" in args.model:
+        if hasattr(model, 'use_contrastive_learning'):
+            model.use_contrastive_learning = args.use_contrastive_learning
+            model.contrastive_temp = args.contrastive_temp
+            model.center_momentum = args.center_momentum
 
     if torch.cuda.device_count() > 1:
         model = nn.DataParallel(model).to(device)
